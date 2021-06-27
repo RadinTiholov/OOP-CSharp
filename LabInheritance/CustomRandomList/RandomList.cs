677f0abdd4a39a87bc03424992391d7cbc263afb
RadinTiholov <radin.tiholov@mail.bg>
2021-06-27 17:23:25 +0300

    Solved the lab

---
+++ LabInheritance/CustomRandomList/RandomList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList:List<string>
+    {
+        public string RandomString() 
+        {
+            Random rnd = new Random();
+            int number = rnd.Next(0, Count);
+            string text = this[number];
+            RemoveAt(number);
+            return text;
+        }
+    }
+}
---
+++ LabInheritance/CustomStack/StackOfStrings.cs
@@ -0,0 +1,25 @@
+﻿namespace CustomStack
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class StackOfStrings:Stack<string>
+    {
+        public bool IsEmpty() 
+        {
+            if (Count == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public void AddRange(params string[] collection) 
+        {
+            foreach (var item in collection)
+            {
+                this.Push(item);
+            }
+        }
+    }
+}
